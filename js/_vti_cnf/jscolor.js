245bdf417538986eaf84b991ceaafedeb99bf50e
Gion <bogdan.gradinariu@form3.tech>
2020-10-06 00:19:33 +0300

    Add proper mobile meta tag and adjust mobile padding

---
+++ js/_vti_cnf/jscolor.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|28 Nov 2017 07:24:02 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_backlinkinfo:VX|script2/script/telepromptersoftware.htm
---
+++ js/_vti_cnf/script.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|07 May 2018 04:52:20 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_backlinkinfo:VX|script2/script/telepromptersoftware.htm
